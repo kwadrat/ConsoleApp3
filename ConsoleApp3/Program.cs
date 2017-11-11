b67e216f2cb71aae0cdb0a04f39b26d96919f4d6
Piotr Kasprzyk <ciri@ciri.pl>
2017-11-11 12:22:10 +0100

    Task 3

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -88,6 +88,32 @@ namespace ConsoleApp3
                     }
                 }
             }
+            /* Zadanie 3 */
+            const int IR_ROWS = 4;
+            int [][] irreg = new int [IR_ROWS][];
+            irreg[0] = new int [4] {1, 2, 3, 4};
+            irreg[1] = new int [2] {5, 6};
+            irreg[2] = new int [3] {7, 8, 9};
+            irreg[3] = new int [1] {10};
+            if(true)
+            {
+                for (int row_nr = 0; row_nr < irreg.Length; row_nr++)
+                {
+                    for (int col_nr = 0; col_nr < irreg[row_nr].Length; col_nr++)
+                    {
+                        Console.Write(irreg[row_nr, col_nr]);
+                        if(col_nr == irreg[row_nr].Length - 1)
+                        {
+                            Console.WriteLine();
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                }
+            }
+            /* Koniec zadan */
             Console.WriteLine("{1} A: {1,3} B: {1,3:F} C: {1,3:e} D: {0} E: {2} F: {3}", "end", 1e-7 / 3.0, sizeof(char), znak);
             Console.WriteLine(Tab);
             Console.WriteLine(koncowy_tekst);
