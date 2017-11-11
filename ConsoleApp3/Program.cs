6830cb9e302a021fbcada895413019b1942396ca
Piotr Kasprzyk <ciri@ciri.pl>
2017-11-11 11:56:04 +0100

    Try 3.1

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -20,19 +20,46 @@ namespace ConsoleApp3
             int[] Tab = new int[SIZE];
             for (int i = 0; i < SIZE; i++)
             {
-                Tab[i] = i * 8;
+                Tab[i] = SIZE - i;
             }
-            for (int i = SIZE - 1; i >= 0; i--)
+            if(1)
             {
-                Console.WriteLine(Tab[i]);
-
+                for (int i = 0; i < SIZE; i++)
+                {
+                    Console.Write(Tab[i]);
+                    if(i == SIZE - 1)
+                    {
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
             }
-            Console.WriteLine("{1}A{1,3}B{1,3:F}C{1,3:e}D{0}\n", "end", 1e-7 / 3.0);
+            for (int i = 0; i < SIZE; i++)
+            {
+                Tab[i] = SIZE - i - 1;
+            }
+            if(1)
+            {
+                for (int i = 0; i < SIZE; i++)
+                {
+                    Console.Write(Tab[i]);
+                    if(i == SIZE - 1)
+                    {
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+            }
+            Console.WriteLine("{1}A{1,3}B{1,3:F}C{1,3:e}D{0}E{2}", "end", 1e-7 / 3.0, sizeof(znak));
             Console.WriteLine(Tab);
             Console.WriteLine(koncowy_tekst);
             Console.ReadKey();
-
-
         }
     }
 }
