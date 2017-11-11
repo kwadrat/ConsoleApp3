3a4cd4d107fdcee01743ebf644d6daffcfb0d291
IEUser <IEUser@IE11WIN7>
2017-11-11 11:04:53 +0100

    Another

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,6 +10,20 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+            int[] Tab = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Tab[i] = i * 7;
+            }
+            for (int i = 9; i >= 0; i--)
+            {
+                Console.WriteLine(Tab[i]);
+
+            }
+            Console.WriteLine("Wiosna\n");
+            Console.ReadKey();
+
+
         }
     }
 }
