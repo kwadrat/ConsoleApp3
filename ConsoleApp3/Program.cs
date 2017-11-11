5411e9cb11ed7f63ea60e76e2214b520f1857b0b
IEUser <IEUser@IE11WIN7>
2017-11-11 11:32:23 +0100

    Add SIZE

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,14 +8,15 @@ namespace ConsoleApp3
 {
     class Program
     {
+        const int SIZE = 20;
         static void Main(string[] args)
         {
-            int[] Tab = new int[10];
-            for (int i = 0; i < 10; i++)
+            int[] Tab = new int[SIZE];
+            for (int i = 0; i < SIZE; i++)
             {
-                Tab[i] = i * 7;
+                Tab[i] = i * 8;
             }
-            for (int i = 9; i >= 0; i--)
+            for (int i = SIZE - 1; i >= 0; i--)
             {
                 Console.WriteLine(Tab[i]);
 
