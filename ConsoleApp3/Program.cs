66070b344315555ce659b479f618f6cb07a1cbbc
Piotr Kasprzyk <ciri@ciri.pl>
2017-11-11 11:42:38 +0100

    Many changes

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,10 +8,15 @@ namespace ConsoleApp3
 {
     class Program
     {
-        const int SIZE = 15;
-        string koncowy_tekst = "Wiosna lato zima\n";
         static void Main(string[] args)
         {
+            const int SIZE = 15;
+            string koncowy_tekst = "Wiosna lato zima\n";
+            int liczba = 2048;
+            double frac = 3.12;
+            Console.WriteLine(liczba);
+            char znak = 'x';
+            Console.WriteLine(frac);
             int[] Tab = new int[SIZE];
             for (int i = 0; i < SIZE; i++)
             {
